8da0a429b0ef3a42d56c235df7407814b05e5184
come1120 <61959057+come1120@users.noreply.github.com>
2020-07-18 02:35:20 +0900

    view_main_update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
+  
   def index
+    @message = Message.new
+    @messages = Message.all.includes(:user).order(created_at: :desc)
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path, notice: 'メッセージが送信されました'
+    else
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
